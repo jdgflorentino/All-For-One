b8377076bd6e1216a702ca1fcb43b458b900caad
Jéssica Florentino <jdgflorentino@gmail.com>
2022-05-30 17:37:04 -0300

    Arrumei 9 e 10.

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
-SELECT * FROM northwind.purchase_orders WHERE create_by >= 3 ORDER BY create_by DESC; 
+SELECT * FROM northwind.purchase_orders WHERE create_by >= 3 ORDER BY create_by DESC, id ASC; 
